d8aa44a2da0f166e31b1d80c26fc00148cbc3a1a
astellon <astellon2525otastepper@gmail.com>
2019-01-28 13:33:09 +0900

    add test

---
+++ test/test.cr
@@ -0,0 +1,14 @@
+require "./sparcr"
+
+include Sparcr
+
+isdigit = Satisfy(Int32).new(->(x : Char) { !x.to_i?.nil? })
+integer = Many.new(isdigit)
+add_op  = Literal.new("+") | Literal.new("-")
+mul_op  = Literal.new("*") | Literal.new("/")
+
+mul_expr = integer  + Many.new( mul_op + integer)
+add_expr = mul_expr + Many.new( add_op + mul_expr)
+expr = add_expr
+
+puts expr.parse("123*456+789").value
